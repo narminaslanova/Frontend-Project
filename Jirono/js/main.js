56d1685fef4bc6acad810cb84ef8c7c2d40da8b4
narminaslanova <narminra@code.edu.az>
2021-02-20 23:43:03 +0400

    firstpage

---
+++ Jirono/js/main.js
@@ -0,0 +1,48 @@
+
+$(function(){
+    $(window).on("load", function () {
+        $(".loader-image").fadeOut("hide");
+      });
+
+    $(document).on("scroll",window,function(){
+        if($(window).scrollTop() > 80){
+           $(".nav-menu").css({
+            "position": "fixed",
+            "background-color": "white",
+            "height": "80px"
+            
+           });
+           $("#up").css({
+               "background":"linear-gradient(135deg, #1A153A 0%, #565178 100%)"
+           })
+        }else{
+            $(".nav-menu").css({
+                "position": "absolute",
+                "background-color": "transparent",
+                "height": "100px"
+               });
+               $("#up").css({
+                   "background":"#1A153A"
+               })
+        }
+    });
+   //counter
+
+    if($("#countTo").length){
+        $('.timer').countTo();
+    }
+
+    if($(window).scrollTop() >100){
+        $(".nav-menu").css({
+            "position": "fixed",
+            "background-color": "white"
+        })
+    };
+
+    new WOW().init();
+
+    $(".parallax-image").parallax("50%", 0.5)
+});
+$(window).on("load", function () {
+    $(".loader-image").fadeOut("slow");
+  });
